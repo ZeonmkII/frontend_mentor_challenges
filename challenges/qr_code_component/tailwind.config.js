4438f1f6e6b0d695b624aaf5a51cf38a6fe5f897
Mathus Tuachob <zeon_mk2@msn.com>
2022-11-24 22:16:44 +0700

    create main qr_component

--- challenges/qr_code_component/tailwind.config.js
+++ challenges/qr_code_component/tailwind.config.js
@@ -1,10 +1,25 @@
 module.exports = {
-  content: ['./challenges/qr_code_component/index.html'],
+  content: ['*.{html,js}'],
   theme: {
+    screens: {
+      sm: '375px',
+      xl: '1440px',
+    },
+    fontFamily: {
+      serif: ['Outfit'],
+    },
+    fontSize: {
+      base: ['15px', '24px'],
+      '2xl': ['24px', '32px'],
+    },
     extend: {
       colors: {
         'optimum-blue': '#009efc',
         'optimum-darkblue': '#0389e1',
+        white: 'hsl(0, 0%, 100%)',
+        'light-gray': 'hsl(212, 45%, 89%)',
+        'grayish-blue': 'hsl(220, 15%, 55%)',
+        'dark-blue': 'hsl(218, 44%, 22%)',
       },
     },
   },
